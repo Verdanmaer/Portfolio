6f5c7541f0d4fce41b5215ef7b6a50b5a5bce03b
Verdanmaer <verneradam618@gmail.com>
2022-04-05 14:47:01 +0200

    add animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-document.getElementsByClassName("header__scroll")[0].addEventListener("click", e => {
-  document.getElementsByClassName("about")[0].scrollIntoView();
+// Scroll on click
+document.getElementsByClassName('header__scroll')[0].addEventListener('click', e => {
+  document.getElementsByClassName('about')[0].scrollIntoView();
+});
+
+// Animate content if it comes into view
+const aboutContent = document.querySelector('.about__content');
+const aboutTechnologies = document.querySelector('.about__technologies');
+const projectsItem1 = document.querySelector('.projects__item--1');
+const projectsItem2 = document.querySelector('.projects__item--2');
+const scrollBreakpoints = [aboutContent, aboutTechnologies, projectsItem1, projectsItem2];
+
+scrollBreakpoints.forEach(breakpoint => {
+  document.addEventListener('scroll', function(e) {
+    const top = window.scrollY + window.innerHeight;
+    const isVisible = top > breakpoint.offsetTop;
+    if (isVisible) breakpoint.classList.add('animate');
+  });
 });
